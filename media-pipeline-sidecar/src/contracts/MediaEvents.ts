6d32b89026df23c449d53b85a7d9d50fb2a37779
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-04-04 01:20:44 -0400

    Add media events

---
+++ media-pipeline-sidecar/src/contracts/MediaEvents.ts
@@ -0,0 +1,9 @@
+export const MediaEvents = {
+  ASSET_CREATED: 'media.asset.created.v1',
+  ASSET_UPDATED: 'media.asset.updated.v1',
+  ASSET_DELETED: 'media.asset.deleted.v1',
+  VARIANT_CREATED: 'media.asset.variant.created.v1',
+  BOUND_AP: 'media.asset.bound.activitypub.v1',
+  BOUND_AT: 'media.asset.bound.atproto.v1',
+  UNREFERENCED: 'media.asset.unreferenced.v1'
+} as const;
